6d1433c652e9cf9c8f5ef5b66ef6ac4d1982eca7
Robert Abreu <robertabreu@pursuit.org>
2019-03-19 21:13:31 -0400

    created base seed file

--- seed.sql
+++ seed.sql
@@ -6,32 +6,57 @@ CREATE DATABASE silkroad;
 CREATE TABLE members
 (
     id SERIAL PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL,
-    PASSWORD VARCHAR NOT NULL,
-    token VARCHAR NULL,
-);
-
-CREATE TABLE posts
-(
-    id SERIAL PRIMARY KEY,
-    member_id INT NOT NULL,
-    caption VARCHAR(255),
-    date_created DATE NOT NULL,
-    date_updated DATE,
-    FOREIGN KEY (member_id) REFERENCES members(id),
-);
-
-CREATE TABLE likes
-(
-    member_id INT NOT NULL,
-    post_id INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES members(id),
-    FOREIGN KEY (post_id) REFERENCES posts(id),
-
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    last_login TIMESTAMP,
+    token VARCHAR
 );
 
 CREATE TABLE messages
 (
+    id SERIAL PRIMARY KEY,
+    member_id INT NOT NULL,
+    message VARCHAR(255),
+    date_created TIMESTAMP NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(id)
+);
 
+CREATE TABLE posts
+( 
+    id SERIAL PRIMARY KEY,
+    member_id INT NOT NULL,
+    caption VARCHAR(255),
+    date_created TIMESTAMP NOT NULL,
+    date_updated TIMESTAMP,
+    CONSTRAINT member_id_fkey 
+    FOREIGN KEY (member_id) REFERENCES members(id)
+    ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE likes
+(
+    id SERIAL PRIMARY KEY,
+    member_id INT NOT NULL,
+    post_id INT NOT NULL,
+    CONSTRAINT member_id_fkey 
+    FOREIGN KEY (member_id) REFERENCES members(id)
+    ON UPDATE NO ACTION ON DELETE CASCADE,
+    CONSTRAINT post_id_fkey 
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+    ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE follow
+(
+    id SERIAL PRIMARY KEY,
+    follower_id INT NOT NULL,
+    followed_id INT NOT NULL,
+    CONSTRAINT follower_id_fkey 
+    FOREIGN KEY (follower_id) REFERENCES members(id)
+    ON UPDATE NO ACTION ON DELETE CASCADE,
+    CONSTRAINT followed_id_fkey 
+    FOREIGN KEY (followed) REFERENCES members(id)
+    ON UPDATE NO ACTION ON DELETE CASCADE
 );
