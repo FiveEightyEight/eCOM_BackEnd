37c0dbef83c5512b809ba7044d5d4800fdb6c860
Robert Abreu <robertabreu@pursuit.org>
2019-03-23 17:12:59 -0400

    created member GET route, requires username in params

--- routes/members.js
+++ routes/members.js
@@ -1,45 +1,76 @@
-const MemberServices =  require('../services/members');
+const MemberServices = require('../services/members');
 const moment = require('moment');
 const validator = require('validator');
 
 const MemberRoutes = require('express').Router();
 
-MemberRoutes.post('/create', (req, res) =>{
-    const {username, email, password} = req.body
-    if(!validator.isEmail(email)) res.status(400).json({error: 'email not valid'});
+MemberRoutes.post('/create', (req, res) => {
+    const {
+        username,
+        email,
+        password
+    } = req.body
+    if (!validator.isEmail(email)) res.status(400).json({
+        error: 'email not valid'
+    });
     // needs username & password validation
     const date_created = moment().format('YYYY-MM-DD hh:mm:ss');
     MemberServices.create(username, email, password, date_created)
-    .then( _ => {
-        res.status(200).json({
-            message: `success, ${username} created`,
-        });
-    })
-    .catch( err => {
-        res.status(400).json({
-            message: `unable to create user, try again`,
-            error: err,
+        .then(_ => {
+            res.status(200).json({
+                message: `success, ${username} created`,
+            });
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `unable to create user, try again`,
+                error: err,
 
+            });
         });
-    });
+});
+
+MemberRoutes.get('/:member_username', (req, res) => {
+    const {
+        member_username
+    } = req.params;
+    MemberServices.read(member_username)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `Could not locate member`,
+                error: err,
+
+            });
+        })
 });
 
 MemberRoutes.put('/update', (req, res) => {
-    const { id, username, email, password } = req.body
-    if(!validator.isEmail(email)) res.status(400).json({error: 'email not valid'});
-    MemberServices.update(id, username, email, password)
-    .then( _=> {
-        res.status(200).json({
-            message: `success, ${username} updated`,
-        });
-    })
-    .catch( err => {
-        res.status(400).json({
-            message: `unable to update user, try again`,
-            error: err,
-
-        });
+    // all fields must be passed in
+    const {
+        id,
+        username,
+        email,
+        password
+    } = req.body
+    if (!validator.isEmail(email)) res.status(400).json({
+        error: 'email not valid'
     });
+    MemberServices.update(id, username, email, password)
+        .then(_ => {
+            res.status(200).json({
+                message: `success, ${username} updated`,
+            });
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `unable to update user, try again`,
+                error: err,
+
+            });
+        });
 });
 
 MemberRoutes.use('/', (req, res) => {
@@ -48,4 +79,6 @@ MemberRoutes.use('/', (req, res) => {
     });
 });
 
-module.exports = {MemberRoutes,};
+module.exports = {
+    MemberRoutes,
+};
