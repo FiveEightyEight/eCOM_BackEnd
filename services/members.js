d8f0fe01edb1d08ed97c30be8c6f3e45f3dc839a
Robert Abreu <robertabreu@pursuit.org>
2019-03-23 16:56:25 -0400

    added update services for members

--- services/members.js
+++ services/members.js
@@ -1,6 +1,8 @@
-const {db,} = require('./db');
+const {
+    db,
+} = require('./db');
 
-const create = (username, email, password, date_created) => {    
+const create = (username, email, password, date_created) => {
     return db.none(`INSERT INTO members (username, email, password, date_created)
         VALUES ($[username], $[email], $[password], $[date_created]);`, {
         username,
@@ -26,10 +28,40 @@ const readToken = (token) => {
 };
 
 const update = (id, username, email, password) => {
+    if (!username && !email && !password) {
+        console.log('UPDATE MEMBER INFO FAILED @ Nothing to update')
+        return new Promise((resolve, reject) => {
+            reject(new Error('Nothing to update'));
+        });
+    };
+    const newValueArr = [username, email, password];
+    const keyNameArr = ['username', 'email', 'password'];
+    let addKey = [];
+    const obj = {
+        id
+    };
 
+    for (let i = 0; i < newValueArr.length; i++) {
+        const key = keyNameArr[i];
+        const val = newValueArr[i]
+        if (!newValueArr[i]) {
+            continue;
+        } else {
+            addKey.push(`${key} = $[${key}]`);
+            obj[key] = val;
+        }
+    };
+    addKey = addKey.join(', ');
+    // console.log('obj', obj)
+    // console.log('addKey: ', addKey)
+    return db.none(`UPDATE members SET ${addKey}
+    WHERE members.id = $[id];`, obj);
 };
 
 
 module.exports = {
     create,
+    read,
+    readToken,
+    update,
 };
