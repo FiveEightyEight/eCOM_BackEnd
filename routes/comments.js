39980a53c27f38e806dfda3c49b24b85891b2b62
Robert Abreu <robertabreu@pursuit.org>
2019-03-24 12:03:50 -0400

    post route to create comment complete, tested manually

--- routes/comments.js
+++ routes/comments.js
@@ -1,4 +1,28 @@
 const CommentRoutes = require('express').Router();
+const CommentServices = require('../services/comments');
+const moment = require('moment');
+
+CommentRoutes.post('/create', (req, res) => {
+    const {
+        member_id,
+        post_id,
+        caption,
+    } = req.body
+    const date_created = moment().format('YYYY-MM-DD hh:mm:ss');
+    CommentServices.create(member_id, post_id, caption, date_created)
+        .then(_ => {
+            res.status(200).json({
+                message: `success, comment created`,
+            });
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `unable to create comment, try again`,
+                error: err,
+
+            });
+        });
+});
 
 CommentRoutes.use('/', (req, res) => {
     res.status(404).json({
