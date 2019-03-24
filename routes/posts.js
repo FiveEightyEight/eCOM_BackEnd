696bbda6be9a52b4d157dd7d4a4ffd1bb80f534c
Robert Abreu <robertabreu@pursuit.org>
2019-03-24 11:09:47 -0400

    create post route created and manually tested

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,28 @@
 const PostRoutes = require('express').Router();
+const PostServices = require('../services/posts');
+const moment = require('moment');
+
+PostRoutes.post('/create', (req, res) => {
+    const {
+        member_id,
+        caption,
+    } = req.body
+    const date_created = moment().format('YYYY-MM-DD hh:mm:ss');
+    PostServices.create(member_id, caption, date_created)
+    .then( _ => {
+        res.status(200).json({
+            message: `success, post created`,
+        });
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: `unable to create post, try again`,
+            error: err,
+
+        });
+    });
+    
+});
 
 PostRoutes.use('/', (req, res) => {
     res.status(404).json({
