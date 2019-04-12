bf5e9d7aa9909b0da5b6f2571af7a7ba49c68914
Robert Abreu <robertabreu@pursuit.org>
2019-04-11 22:06:52 -0400

    installed cors lib

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const {MemberRoutes,} = require('./routes/members');
 const {PostRoutes,} = require('./routes/posts');
 const {CommentRoutes,} = require('./routes/comments');
@@ -10,12 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-  });
+app.use(cors())
 
 app.use('/members', MemberRoutes);
 app.use('/posts', PostRoutes);
